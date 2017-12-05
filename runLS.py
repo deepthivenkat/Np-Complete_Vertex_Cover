e3ac5a8db3e597fb67cef4f50443bdc70e6a3d74
winky94 <atwinkles@mindspring.com>
2017-12-05 18:41:21 -0500

    finished obtaining results

--- runLS.py
+++ runLS.py
@@ -14,10 +14,10 @@ def runLS1():
 
 def test():
     thepath = inpath + "karate.graph"
-    otherpath = "../Data/power.graph"
-    for j in range(1,11):
-        for k in range(1,11):
-            timeval = j * 60
-            seedval = int(random.random() * 1000)
-            stringval = "python runproject.py -inst %s -alg LS1 -time %d -seed %d" % (otherpath, timeval, seedval)
-            os.system(stringval)
+    otherpath = "../Data/as-22july06.graph"
+    #for j in range(9,11):
+    for k in range(1,3):
+            #timeval = j * 60
+        seedval = int(random.random() * 1000)
+        stringval = "python runproject.py -inst %s -alg LS1 -time %d -seed %d" % (otherpath, 480, seedval)
+        os.system(stringval)
