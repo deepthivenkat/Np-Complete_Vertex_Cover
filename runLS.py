a4a40156a483e7e145ef7d0b46f74bfba4a8bb7f
winky94 <atwinkles@mindspring.com>
2017-12-04 23:24:15 -0500

    finished power computation

--- runLS.py
+++ runLS.py
@@ -14,10 +14,10 @@ def runLS1():
 
 def test():
     thepath = inpath + "karate.graph"
-    otherpath = "../Data/karate.graph"
+    otherpath = "../Data/power.graph"
     for j in range(1,11):
-        #timeval = j * 60
-        seedval = int(random.random() * 100)
-        stringval = "python runproject.py -inst %s -alg LS1 -time %d -seed %d" % (otherpath, 60, seedval)
-        print stringval
-        #os.system(stringval)
+        for k in range(1,11):
+            timeval = j * 60
+            seedval = int(random.random() * 1000)
+            stringval = "python runproject.py -inst %s -alg LS1 -time %d -seed %d" % (otherpath, timeval, seedval)
+            os.system(stringval)
